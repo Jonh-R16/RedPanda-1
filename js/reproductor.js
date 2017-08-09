4d1f017ea5d61de270985edfc00b21b0ec00c1d5
unknown <katerozarack@gmail.com>
2017-08-09 10:35:42 -0600

    avance javascript

--- js/reproductor.js
+++ js/reproductor.js
@@ -1,5 +1,21 @@
-function reproducir(){
-	var m = document.getElementById("archivo").value;
-	//m.src=document.getElementById("archivo").value;
-	alert(m);
+window.onload=function(){
+	var audio=document.getElementById("audio");
+	audio.volume=document.getElementById("volumen").value;
+	document.getElementById("play").onclick=function(){
+		audio.play();
+	}
+	document.getElementById("pause").onclick=function(){
+		audio.pause();
+	}
+	document.getElementById("stop").onclick=function(){
+		audio.load();
+	}
+	document.getElementById("volumen").onchange=function(e){
+		audio.volume=e.target.value;
+	}
+	audio.ontimeupdate=function(){
+		document.getElementById("tiempo").max=audio.duration;
+		document.getElementById("tiempo").value=audio.currentTime;
+	}
+	document.getElementById("cancion").innerHTML=audio.currentSrc;
 }
